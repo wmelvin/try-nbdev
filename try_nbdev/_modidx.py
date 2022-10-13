ff127fa9bea343b77ee7ec2ff70d550f40703626
Bill Melvin <bill@billmelvin.com>
2022-10-13 12:39:20 -0400

    Implement hello.py.

--- try_nbdev/_modidx.py
+++ try_nbdev/_modidx.py
@@ -5,4 +5,4 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://wmelvin.github.io',
                 'git_url': 'https://github.com/wmelvin/try-nbdev',
                 'lib_path': 'try_nbdev'},
-  'syms': {'try_nbdev.core': {'try_nbdev.core.foo': ('core.html#foo', 'try_nbdev/core.py')}}}
+  'syms': {'try_nbdev.hello': {'try_nbdev.hello.say_hello': ('hello.html#say_hello', 'try_nbdev/hello.py')}}}
